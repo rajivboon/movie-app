82e9362ac9069c3218b4b2e522d200d6d0e69bd2
rajivboon <vrajivboon@gmail.com>
2020-09-23 01:16:58 +0530

    6. Class Components

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 // functional component - arrow function
+// WHEN TO USE:
+// 1. For smaller loadComponents
+// 2. reusable components 
+// 3. presentional loadComponents, patialy RTCIceGatherer, we can use HOOKS and specify state 
 // const About = () => {
 //     const message = 'Hello world'
 //     return (
@@ -8,13 +12,13 @@ import React from 'react';
 //     )
 // }
 
-const About = () => {
-    const message = 'surya is a good boy'
-    return React.createElement('h1', null, 'who is good boy {$message} ' )
-}
+class About extends React.Component {
 
-// const About = () => (
-//     <h1>Hello About Page</h1>
-// )
+    render() {
+        return (
+            <h1> Hello I am class component</h1>
+        )
+    }
+}
 
 export default About
