67947c797665fea05eed00707ff5136d7bb8ff21
rajivboon <vrajivboon@gmail.com>
2020-09-26 23:57:21 +0530

    15 A. Iterate over list of data

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -2,19 +2,13 @@
 const SideMenu = (props) => {
    
     return (
-        <div>
-            <button onClick={props.clickHandler} >Click Me</button>
+        <div>            
             <h1 className="my-4">{props.appName}</h1>
             <div className="list-group">
             <a href="#" className="list-group-item">Category 1</a>
             <a href="#" className="list-group-item">Category 2</a>
             <a href="#" className="list-group-item">Category 3</a>
-            </div>
-
-            <div>
-            
-            <h1> {props.whatEver} </h1>
-            </div>
+            </div>           
         </div>
 
     )
